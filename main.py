895bdb6033857ce64dab62d373539f90bd816818
Walentin <valoliinykov.work@gmail.com>
2024-12-04 10:13:32 +0200

    add readme

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import os
+from datetime import datetime
+from functools import lru_cache
+
 import uvicorn
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile, File, Query
+from fastapi.responses import FileResponse
 
 
 app = FastAPI(docs_url="/docs")
@@ -17,7 +21,20 @@ async def upload_image(file: UploadFile = File(..., description="upload any file
     with open(path_file, "wb") as buffer:
         buffer.write(content_file)
 
+    with open(path_file, "a") as buffer:
+        current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        buffer.write(f"file uploaded on: {current_date}")
+
     return {"file": file, "about": f'{file.filename} uploaded'}
+
+
+@app.get("/file/download")
+@lru_cache
+def download_file(filename: str = Query(..., description="Enter filename for downloads")):
+    file_path = os.path.join(UPLOAD_DIR, filename)
+    return FileResponse(path=file_path,
+                        filename='New file from service.txt',
+                        media_type='multipart/form-data')
 
 
 if __name__ == "__main__":
